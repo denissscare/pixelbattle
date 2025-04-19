b8f42e6cf57a9c9c478df1326af85fa718564490
Denis <denissscare@gmail.com>
2025-04-19 21:33:02 +0800

    feat(server): add graceful shutdown realization

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"pixelbattle/internal/config"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -12,6 +17,10 @@ type Server struct {
 	cfg    *config.Config
 	router *chi.Mux
 }
+
+const (
+	gracefulTime int = 15
+)
 
 func New(cfg *config.Config, router *chi.Mux) *Server {
 	return &Server{
@@ -31,10 +40,25 @@ func (s *Server) Run() error {
 	}
 
 	go func() {
+		fmt.Printf("\n\nSERVER START ON: %v:%d\n\n", s.cfg.Server.Host, s.cfg.Server.Port)
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			panic("failed to start server: " + err.Error())
 		}
 	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+	fmt.Printf("\n\nSERVER STOPPING\n\n")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(gracefulTime)*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		panic("failed to stop server")
+	}
+	fmt.Printf("\nSERVER STOP\n")
 
 	return nil
 }
