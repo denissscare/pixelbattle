c0c1308c8f1e8a5c7c82abb7d52a825775e87441
Denis <denissscare@gmail.com>
2025-05-03 02:28:37 +0800

    feat(pixcelbattle): update logs to pixel handler

--- backend/internal/pixcelbattle/handlers/pixel.go
+++ backend/internal/pixcelbattle/handlers/pixel.go
@@ -6,33 +6,22 @@ import (
 	"pixelbattle/internal/pixcelbattle/domain"
 	"pixelbattle/internal/pixcelbattle/service"
 	"pixelbattle/pkg/logger"
-
-	"github.com/sirupsen/logrus"
 )
 
 func UpdatePixelHandler(svc *service.BattleService, log *logger.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var p domain.Pixel
 		if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
-			log.WithFields(logrus.Fields{
-				"action":    "json.NewDecoder",
-				"component": "pixcelbatlle.handlers.UpdatePixelHandler",
-				"success":   false,
-			}).Errorf("bad request payload: %s", err)
+			log.Errorf("HTTP POST /pixel bad payload: %v", err)
 			http.Error(w, "invalid JSON payload", http.StatusBadRequest)
 			return
 		}
-
 		if err := svc.UpdatePixel(r.Context(), p); err != nil {
-			log.WithFields(logrus.Fields{
-				"action":    "svc.UpdatePixel",
-				"component": "pixcelbatlle.handlers.UpdatePixelHandler",
-				"success":   false,
-			}).Errorf("bad request payload: %s", err)
+			log.Errorf("HTTP POST /pixel update failed: %v", err)
 			http.Error(w, "failed to update pixel", http.StatusInternalServerError)
 			return
 		}
-
 		w.WriteHeader(http.StatusNoContent)
+		log.Infof("HTTP POST /pixel → updated pixel at (%d,%d) by %s", p.X, p.Y, p.Author)
 	}
 }
