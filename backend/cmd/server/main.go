fc7d716c3203771678b3b4e99ed5dba6854eff3d
Denis <denissscare@gmail.com>
2025-05-03 01:05:16 +0800

    feat(main): init the service and connect routes

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"context"
 	"pixelbattle/internal/config"
+	"pixelbattle/internal/pixcelbattle/broker"
+	"pixelbattle/internal/pixcelbattle/service"
+	"pixelbattle/internal/pixcelbattle/storage/redis"
 	"pixelbattle/internal/server"
 	"pixelbattle/pkg/logger"
 )
 
 func main() {
+	ctx := context.Background()
 	config := config.LoadConfig()
 	log := logger.New(config.Environment)
 
-	router := server.InitRouter()
+	rds, err := redis.NewClient(ctx, *config)
+	if err != nil {
+		log.Fatalf("Redis init failed: %v", err)
+	}
+	defer rds.Close(ctx)
+
+	br, err := broker.NewBroker(config.NATS.URL)
+	if err != nil {
+		log.Fatalf("NATS init failed: %v", err)
+	}
+	defer br.Close()
+
+	pixelbattle := service.NewBattleService(*rds, *br, log)
+
+	router := server.InitRouter(pixelbattle, log)
+
 	srv := server.New(config, router, log)
 
 	if err := srv.Run(); err != nil {
-		panic("failed start")
+		log.Fatalf("Server shutdown error: %v", err)
 	}
 }
