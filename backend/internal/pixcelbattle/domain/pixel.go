e4ac000f0838f27535a18e0229ab7daa03e858ab
Denis <denissscare@gmail.com>
2025-05-03 03:05:51 +0800

    feat(domain): add the validator for pixcels

--- backend/internal/pixcelbattle/domain/pixel.go
+++ backend/internal/pixcelbattle/domain/pixel.go
@@ -1,11 +1,66 @@
 package domain
 
-import "time"
+import (
+	"fmt"
+	"regexp"
+	"time"
+)
+
+var (
+	MaxWidth  = 1000
+	MaxHeight = 1000
+	re        = regexp.MustCompile(`(?i)^#[0-9A-F]{6}$`)
+)
 
 type Pixel struct {
 	X         int       `json:"x"`
 	Y         int       `json:"y"`
-	Color     string    `json:"color"`     // "#RRGGBB"
-	Author    string    `json:"author"`    // кто изменил
-	Timestamp time.Time `json:"timestamp"` // когда изменил
+	Color     string    `json:"color"`
+	Author    string    `json:"author"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+func (p Pixel) Validate() error {
+	var errs ValidationErrors
+
+	if p.X < 0 || p.X >= MaxWidth {
+		errs = append(errs, ValidationError{
+			Field:   "X",
+			Message: fmt.Sprintf("out of range [0,%d)", MaxWidth),
+		})
+	}
+	if p.Y < 0 || p.Y >= MaxHeight {
+		errs = append(errs, ValidationError{
+			Field:   "Y",
+			Message: fmt.Sprintf("out of range [0,%d)", MaxHeight),
+		})
+	}
+	if !re.MatchString(p.Color) {
+		errs = append(errs, ValidationError{
+			Field:   "Color",
+			Message: "must be hex #RRGGBB",
+		})
+	}
+	if p.Author == "" {
+		errs = append(errs, ValidationError{
+			Field:   "Author",
+			Message: "cannot be empty",
+		})
+	}
+	if p.Timestamp.IsZero() {
+		errs = append(errs, ValidationError{
+			Field:   "Timestamp",
+			Message: "must be set",
+		})
+	} else if p.Timestamp.After(time.Now().Add(time.Minute)) {
+		errs = append(errs, ValidationError{
+			Field:   "Timestamp",
+			Message: "too far in the future",
+		})
+	}
+
+	if errs.HasErrors() {
+		return errs
+	}
+	return nil
 }
