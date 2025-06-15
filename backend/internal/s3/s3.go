793f2c23562a2e883ee48c9c54e6e262774907d1
Denis <denissscare@gmail.com>
2025-06-16 02:43:36 +0800

    feat(s3): add the s3 constructor

---
+++ backend/internal/s3/s3.go
@@ -0,0 +1,57 @@
+package s3
+
+import (
+	"context"
+	"mime/multipart"
+	"pixelbattle/internal/config"
+	"time"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+type Client struct {
+	cli    *minio.Client
+	bucket string
+}
+
+func New(cfg config.Config) (*Client, error) {
+	cli, err := minio.New(cfg.Minio.Endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(cfg.Minio.AccessKey, cfg.Minio.SecretKey, ""),
+		Secure: cfg.Minio.UseSSL,
+	})
+	if err != nil {
+		return nil, err
+	}
+	ctx := context.Background()
+
+	err = cli.MakeBucket(ctx, cfg.Minio.Bucket, minio.MakeBucketOptions{})
+	if err != nil {
+		exists, errBucketExists := cli.BucketExists(ctx, cfg.Minio.Bucket)
+		if errBucketExists != nil || !exists {
+			return nil, err
+		}
+	}
+	return &Client{cli: cli, bucket: cfg.Minio.Bucket}, nil
+}
+
+func (c *Client) UploadFile(ctx context.Context, fileHeader *multipart.FileHeader, objectName string) (string, error) {
+	file, err := fileHeader.Open()
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	_, err = c.cli.PutObject(ctx, c.bucket, objectName, file, fileHeader.Size, minio.PutObjectOptions{ContentType: fileHeader.Header.Get("Content-Type")})
+	if err != nil {
+		return "", err
+	}
+	return objectName, nil
+}
+
+func (c *Client) GetPresignedURL(ctx context.Context, objectName string, expires time.Duration) (string, error) {
+	u, err := c.cli.PresignedGetObject(ctx, c.bucket, objectName, expires, nil)
+	if err != nil {
+		return "", err
+	}
+	return u.String(), nil
+}
