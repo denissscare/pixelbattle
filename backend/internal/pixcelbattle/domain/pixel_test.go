cb4b75d6293fe6e6418190e1c3a5594264493298
igulyaew <114748738+igulyaew@users.noreply.github.com>
2025-06-16 19:04:45 +0800

    Create pixel_test.go

---
+++ backend/internal/pixcelbattle/domain/pixel_test.go
@@ -0,0 +1,38 @@
+package domain
+
+import (
+  "github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestValidate(t *testing.T) {
+  
+  p := Pixel {
+  	    X:         MaxWidth + 10,
+  	    Y:         20,
+  	    Color:     "#FF0000",
+  	    Author:    "user123",
+  	    Timestamp: time.Now(),
+  }
+  require.Error(t, p.Validate())
+
+  p := Pixel {
+      X:         10,
+      Y:         20,
+      Color:     "#FF0000",
+      Author:    "",
+      Timestamp: time.Now(),
+  }
+  require.Error(t, p.Validate())
+  
+  p := Pixel {
+      X:         10,
+      Y:         20,
+      Color:     "#FF0000",
+      Author:    "artist",
+      Timestamp: time.Now().Add(time.Hour),
+  }
+  require.Error(t, p.Validate())
+  
+}
