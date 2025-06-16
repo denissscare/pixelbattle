69d251f0809b019f83332a5dd325df4c1c1b9b50
igulyaew <114748738+igulyaew@users.noreply.github.com>
2025-06-16 18:47:17 +0800

    Update battle_test.go

--- backend/internal/pixcelbattle/service/battle_test.go
+++ backend/internal/pixcelbattle/service/battle_test.go
@@ -7,9 +7,11 @@ import (
 	"pixelbattle/pkg/logger"
 	"pixelbattle/internal/pixcelbattle/metrics"
 	"pixelbattle/internal/pixcelbattle/broker"
+	"pixelbattle/internal/pixcelbattle/domain"
 	
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func TestNewBattleService(t *testing.T) {
@@ -35,4 +37,21 @@ func TestInitCanvas(t *testing.T) {
 	bs := TestNewBattleService(t)
 	_, err := bs.InitCanvas(ctx)
 	require.NoError(t, err)
+}
+
+func TestUpdatePixel(t *testing.T) {
+	ctx := context.Background()
+
+	p := domain.Pixel {
+	    X:         10,
+	    Y:         20,
+	    Color:     "#FF0000",
+	    Author:    "user123",
+	    Timestamp: time.Now(),
+	}
+
+	require.Nil(t, p.Validate())
+
+	bs := TestNewBattleService(t)
+	require.Nil(t, bs.UpdatePixel(ctx, p))
 }
